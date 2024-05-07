012cec2aa96132e5371a76d89be05daee987e6ea
xujiahe0529 <xujiahe0529@outlook.com>
2024-05-07 09:31:54 +0800

    feat:framework complete

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,60 @@
           <el-button @click="addWidget" class="add-button">添加任务</el-button>
         </div>
         <el-collapse class="collapse-container">
-          <el-collapse-item v-for="(widget, index) in widgets" :key="widget.id" :title=" '任务' + (index + 1)"
+          <el-collapse-item v-for="(widget, ) in widgets" :key="widget.id" :title="widget.title"
                             class="collapse-item">
-            <el-form label-width="auto" class="collapse-el-item">
-              <el-form-item v-if="widget.type === 'text'" label="文本内容">
+            <el-form label-position="left" class="collapse-el-item">
+              <el-form-item v-if="widget.type === '1'" label="选择类别">
+                <el-select v-model="widget.settings.value">
+                  <el-option label="军事" value="military"></el-option>
+                  <el-option label="科技" value="science"></el-option>
+                  <el-option label="金融" value="finance"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '1'" label="开启攒批">
+                <el-checkbox v-model="widget.settings.checked"></el-checkbox>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '2'" label="模型选择（多选）">
+                <el-select v-model="widget.settings.value">
+                  <el-option label="线性回归" value="1"></el-option>
+                  <el-option label="随机梯度下降" value="2"></el-option>
+                  <el-option label="朴素贝叶斯" value="3"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '2'" label="选择价值范围">
+                <el-select v-model="widget.settings.value">
+                  <el-option label="低" value="low"></el-option>
+                  <el-option label="中" value="medium"></el-option>
+                  <el-option label="高" value="high"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '2'" label="开启查询内冗余消除">
+                <el-checkbox v-model="widget.settings.checked"></el-checkbox>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '3'" label="选择模型加速策略">
+                <el-select v-model="widget.settings.value">
+                  <el-option label="剪枝" value="1"></el-option>
+                  <el-option label="量化" value="2"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '3'" label="开启模型加载自动外提">
+                <el-checkbox v-model="widget.settings.checked"></el-checkbox>
+              </el-form-item>
+              <el-form-item v-if="widget.type === '4'" label="新闻选择">
                 <el-input v-model="widget.settings.text"></el-input>
               </el-form-item>
-              <el-form-item v-if="widget.type === 'text'" label="字体大小">
-                <el-input v-model="widget.settings.fontSize"></el-input>
+              <el-form-item v-if="widget.type === '4'" label="选择价值范围">
+                <el-select v-model="widget.settings.value">
+                  <el-option label="低" value="low"></el-option>
+                  <el-option label="中" value="medium"></el-option>
+                  <el-option label="高" value="high"></el-option>
+                </el-select>
               </el-form-item>
-              <el-form-item v-if="widget.type === 'image'" label="图像链接">
-                <el-input v-model="widget.settings.imageUrl"></el-input>
+              <el-form-item v-if="widget.type === '4'" label="返回新闻条数">
+                <el-input v-model="widget.settings.text"></el-input>
               </el-form-item>
-              <el-form-item v-if="widget.type === 'image'" label="宽度">
-                <el-input v-model="widget.settings.width"></el-input>
+              <el-form-item v-if="widget.type === '4'" label="开启查询间冗余消除">
+                <el-checkbox v-model="widget.settings.checked"></el-checkbox>
               </el-form-item>
             </el-form>
             <div class="execute-button-div">
@@ -41,13 +81,13 @@
       <!-- 右侧布局 -->
       <el-main class="right-main">
         <el-container direction="vertical">
-          <el-header style="height: 50px;">表格</el-header>
+          <el-header style="height: 50px;">查询结果</el-header>
           <el-main class="table-main">
             <el-table :data="tableData" border>
               <el-table-column prop="news" label="新闻"></el-table-column>
             </el-table>
           </el-main>
-          <el-header style="height: 50px;">图表</el-header>
+          <el-header style="height: 50px;">优化效果</el-header>
           <el-footer class="chart-footer">
             <!-- 底部图表 -->
             <div class="chart-box" v-for="(chartData, index) in chartDatas" :key="index">
@@ -76,8 +116,10 @@ export default {
       widgets: [], // 保存小组件的数组
       selectedComponentType: '', // 保存当前选中的组件类型
       componentTypes: [ // 定义组件类型
-        { label: '文本组件', value: 'text' },
-        { label: '图像组件', value: 'image' }
+        { label: '任务1：新闻类别筛选', value: '1' },
+        { label: '任务2：新闻价值评价', value: '2' },
+        { label: '任务3：新闻摘要生成', value: '3' },
+        { label: '任务4：新闻兴趣推荐', value: '4' },
       ],
       widgetIdCounter: 0 // 小组件 ID 计数器
     };
@@ -89,12 +131,19 @@ export default {
         type: this.selectedComponentType,
         settings: {}
       };
-      if (this.selectedComponentType === 'text') {
-        newWidget.settings.text = '';
+      if (this.selectedComponentType === '1') {        //修改settings值
         newWidget.settings.fontSize = '';
-      } else if (this.selectedComponentType === 'image') {
-        newWidget.settings.imageUrl = '';
+      } else if (this.selectedComponentType === '2') {
         newWidget.settings.width = '';
+      } else if (this.selectedComponentType === '3') {
+        newWidget.settings.width = '';
+      } else if (this.selectedComponentType === '4') {
+        newWidget.settings.width = '';
+      }
+      // 根据选中的组件类型，设置对应的标题
+      const selectedType = this.componentTypes.find(type => type.value === this.selectedComponentType);
+      if (selectedType) {
+        newWidget.title = selectedType.label;
       }
       this.widgets.push(newWidget);
     },
