e57cae0b4d97bc1194e665943ce8d4202dd71704
xujiahe0529 <xujiahe0529@outlook.com>
2024-05-10 15:02:22 +0800

    feat:complete sql + show table

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <el-container>
       <!-- 左侧布局 -->
       <el-aside class="left-aside">
+        <div class="execute-button-div">
+          <el-button @click="showData" class="add-button">获取新闻数据</el-button>
+        </div> 
         <el-select v-model="selectedComponentType" placeholder="选择任务类型">
           <el-option
               v-for="(type, index) in componentTypes"
@@ -18,16 +21,22 @@
           <el-collapse-item v-for="(widget, ) in widgets" :key="widget.id" :title="widget.title"
                             class="collapse-item">
             <el-form label-position="left" class="collapse-el-item">
+              <!-- 任务1 -->
               <el-form-item v-if="widget.type === '1'" label="选择类别">
                 <el-select v-model="widget.settings.value">
                   <el-option label="军事" value="military"></el-option>
-                  <el-option label="科技" value="science"></el-option>
-                  <el-option label="金融" value="finance"></el-option>
+                  <el-option label="非军事" value="non_military"></el-option>
                 </el-select>
               </el-form-item>
               <el-form-item v-if="widget.type === '1'" label="开启攒批">
                 <el-checkbox v-model="widget.settings.checked"></el-checkbox>
               </el-form-item>
+              <el-form-item v-if="widget.type === '1'" label="SQL语句：">
+              </el-form-item>
+              <el-form-item v-if="widget.type === '1'">
+                <el-input type="textarea" :rows="5" :value="widget.sql"></el-input>
+              </el-form-item>
+              <!-- 任务2 -->
               <el-form-item v-if="widget.type === '2'" label="模型选择（多选）">
                 <el-select v-model="widget.settings.value">
                   <el-option label="线性回归" value="1"></el-option>
@@ -45,6 +54,7 @@
               <el-form-item v-if="widget.type === '2'" label="开启查询内冗余消除">
                 <el-checkbox v-model="widget.settings.checked"></el-checkbox>
               </el-form-item>
+              <!-- 任务3 -->
               <el-form-item v-if="widget.type === '3'" label="选择模型加速策略">
                 <el-select v-model="widget.settings.value">
                   <el-option label="剪枝" value="1"></el-option>
@@ -54,6 +64,12 @@
               <el-form-item v-if="widget.type === '3'" label="开启模型加载自动外提">
                 <el-checkbox v-model="widget.settings.checked"></el-checkbox>
               </el-form-item>
+              <el-form-item v-if="widget.type === '3'" label="SQL语句：">
+              </el-form-item>
+              <el-form-item v-if="widget.type === '3'">
+                <el-input type="textarea" :rows="5" :value="widget.sql"></el-input>
+              </el-form-item>
+              <!-- 任务4 -->
               <el-form-item v-if="widget.type === '4'" label="新闻选择">
                 <el-input v-model="widget.settings.text"></el-input>
               </el-form-item>
@@ -72,7 +88,7 @@
               </el-form-item>
             </el-form>
             <div class="execute-button-div">
-              <el-button @click="updateChart" class="execute-button">执行</el-button>
+              <el-button @click="updateChart(widget)" class="execute-button">执行</el-button>
             </div>
           </el-collapse-item>
         </el-collapse>
@@ -84,7 +100,7 @@
           <el-header style="height: 50px;">查询结果</el-header>
           <el-main class="table-main">
             <el-table :data="tableData" border>
-              <el-table-column prop="news" label="新闻"></el-table-column>
+              <el-table-column v-for="(value, key) in tableColumns" :key="key" :prop="key" :label="key"></el-table-column>
             </el-table>
           </el-main>
           <el-header style="height: 50px;">优化效果</el-header>
@@ -102,7 +118,7 @@
 
 <script>
 
-import { tableData, chartDatas } from './data/data.js';
+import { tableData, chartDatas,NewsData,inferenceResultsData } from './data/data.js';
 import * as echarts from 'echarts'
 
 export default {
@@ -111,7 +127,8 @@ export default {
   },
   data() {
     return {
-      tableData: tableData,
+      tableData: [],     //表格数据
+      tableColumns: [],   //表格属性列
       chartDatas: chartDatas,
       widgets: [], // 保存小组件的数组
       selectedComponentType: '', // 保存当前选中的组件类型
@@ -129,10 +146,12 @@ export default {
       const newWidget = {
         id: this.widgetIdCounter++, // 为小部件生成唯一 ID
         type: this.selectedComponentType,
-        settings: {}
+        settings: {},
+        sql:''      //保存拼装的SQL语句
       };
-      if (this.selectedComponentType === '1') {        //修改settings值
-        newWidget.settings.fontSize = '';
+
+      //根据不同的任务，拼装不同的SQL
+      if (this.selectedComponentType === '1') {        //修改settings值，拼装对应的SQL
       } else if (this.selectedComponentType === '2') {
         newWidget.settings.width = '';
       } else if (this.selectedComponentType === '3') {
@@ -163,6 +182,29 @@ export default {
           type: 'bar'
         }]
       })
+    },
+    //获取新闻数据按钮，显示所有新闻数据
+    showData() {
+      // console.log(tableData);
+      this.tableData = NewsData;
+      this.tableColumns = Object.values(NewsData[0]);
+      console.log(this.tableData);
+      console.log(this.tableColumns);
+    },
+    //执行按钮，显示执行结果
+    updateChart(widget) {
+      if (widget.type === '1') {
+        const categorySQL = `SELECT title, source, content FROM news WHERE PREDICT is_military(content) = "${widget.settings.value}";`;
+        widget.sql = categorySQL;
+        console.log(categorySQL);
+      } else if (widget.type === '2') {
+      // 根据其他组件类型继续拼装对应的 SQL 语句
+      } else if (widget.type === '3') {
+        const categorySQL = `SELECT PREDICT summary(text) AS summary FROM important_news;`;
+        widget.sql = categorySQL;
+      } else if (widget.type === '4') {
+      // 根据其他组件类型继续拼装对应的 SQL 语句
+      }
     }
     // 其他方法...
   }
@@ -190,6 +232,7 @@ export default {
 }
 
 .execute-button-div{
+  margin-bottom: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -208,7 +251,7 @@ export default {
 }
 
 .collapse-el-item{
-  padding: 20px;
+  padding: 10px;
 }
 
 .right-main {
